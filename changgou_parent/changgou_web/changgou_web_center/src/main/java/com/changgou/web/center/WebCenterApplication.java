0338f219a2cfeb5c8442d4887f005e0eb3c994c5
xyz <529165167@qq.com>
2020-06-21 13:45:47 +0800

    docs: 修改端口

--- changgou_parent/changgou_web/changgou_web_center/src/main/java/com/changgou/web/center/WebCenterApplication.java
+++ changgou_parent/changgou_web/changgou_web_center/src/main/java/com/changgou/web/center/WebCenterApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebCenterApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebCenterApplication.class,args);
     }
