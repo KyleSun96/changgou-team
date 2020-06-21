0df0117bfa658a8092d7cfb94f199a972e38f656
xuewj1492 <xuewj1492@163.com>
2020-06-21 17:07:53 +0800

    新增商品收藏功能

--- changgou_parent/changgou_service/changgou_service_search/src/main/java/com/changgou/search/SearchApplication.java
+++ changgou_parent/changgou_service/changgou_service_search/src/main/java/com/changgou/search/SearchApplication.java
@@ -1,9 +1,11 @@
 package com.changgou.search;
 
+import com.changgou.interceptor.FeignInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,5 +14,11 @@ public class SearchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class,args);
+    }
+
+
+    @Bean
+    public FeignInterceptor feignInterceptor(){
+        return new FeignInterceptor();
     }
 }
