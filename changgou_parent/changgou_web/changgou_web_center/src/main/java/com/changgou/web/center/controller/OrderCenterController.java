48199be87bc546216e76a0198b3566fdcef94d86
xuewj1492 <xuewj1492@163.com>
2020-06-22 16:15:13 +0800

    新增我的足迹功能

--- changgou_parent/changgou_web/changgou_web_center/src/main/java/com/changgou/web/center/controller/OrderCenterController.java
+++ changgou_parent/changgou_web/changgou_web_center/src/main/java/com/changgou/web/center/controller/OrderCenterController.java
@@ -1,5 +1,10 @@
 package com.changgou.web.center.controller;
 
+import com.changgou.entity.Result;
+import com.changgou.goods.feign.FavoritesFeign;
+import com.changgou.goods.feign.FootmarkFeign;
+import com.changgou.goods.pojo.Favorites;
+import com.changgou.goods.pojo.Footmark;
 import com.changgou.order.feign.OrderFeign;
 import com.changgou.order.feign.OrderItemFeign;
 import com.changgou.order.pojo.Order;
@@ -19,6 +24,12 @@ public class OrderCenterController {
 
     @Autowired
     private OrderItemFeign orderItemFeign;
+
+    @Autowired
+    private FavoritesFeign favoritesFeign;
+
+    @Autowired
+    private FootmarkFeign footmarkFeign;
 
     //确定收货
     @RequestMapping("/define")
@@ -63,6 +74,24 @@ public class OrderCenterController {
         model.addAttribute("orderList", orderList);
 
         return "center-order-evaluate";
+    }
+
+
+    //跳转收藏列表页面
+    @RequestMapping("/toCollect")
+    public String toCollect(Model model) {
+        List<Favorites> favoritesList = favoritesFeign.list().getData();
+        model.addAttribute("favoritesList", favoritesList);
+        return "center-collect";
+    }
+
+
+    //跳转我的足迹页面
+    @RequestMapping("/toFootmark")
+    public String toFootmark(Model model) {
+        List<Footmark> footmarkList = footmarkFeign.list().getData();
+        model.addAttribute("footmarkList", footmarkList);
+        return "center-footmark";
     }
 
 }
