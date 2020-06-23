dbc2a31e26f87ab6f33fd2d5f004e64a5ef2d95f
KyleSun <swy0907163@163.com>
2020-06-23 14:21:38 +0800

    feat: task定时刷新（ing）

---
+++ changgou_parent/changgou_service/changgou_service_comment/src/main/java/com/changgou/comment/task/CommentTask.java
@@ -0,0 +1,58 @@
+package com.changgou.comment.task;
+
+import com.changgou.comment.dao.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @Program: changgou_parent
+ * @ClassName: CommentTask
+ * @Description: 用户评论定时扫描
+ * @Author: KyleSun
+ **/
+@Component
+public class CommentTask {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    // 两分钟更新一次
+    @Scheduled(cron = "0 0/2 * * * ?")
+    public void updateCommentCount() {
+
+//        // 更新获取spu最新评价数量
+//        Aggregation agg = Aggregation.newAggregation(Aggregation.group("spuId"));
+//        AggregationResults<String> aggregationResults = mongoTemplate.aggregate(agg, "comment", String.class);
+//
+//        List<String> spuIdList = aggregationResults.getMappedResults();
+//        System.out.println(spuIdList);
+//
+//        if (spuIdList.size() <= 0) {
+//            throw new RuntimeException("当前商品无评价信息");
+//        }
+//
+//        spuIdList.stream().forEach((spuId) -> {
+//            // 根据spuId查询评论数量
+//            Long count = commentRepository.countAllBySpuId(spuId);
+//
+//            // 存入redis
+//            redisTemplate.opsForValue().set(spuId, count);
+//
+//        });
+    }
+
+
+}
